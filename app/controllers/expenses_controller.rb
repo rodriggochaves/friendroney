4d4b1c1205e84559e9f092c2171e1a0afeed76e6
rodriggochaves <rodriggo.chaves@gmail.com>
2016-07-30 13:04:44 -0300

    teste que valida a criação de uma "expense"

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,10 +1,11 @@
 class ExpensesController < ApplicationController
   def new
+    @expense = Expense.new
     @expenses = Expense.all
   end
 
   def create
-    expense = Expense.new params.permit(:value, :description)
+    expense = Expense.new params.require(:expense).permit(:value, :description)
     if expense.save
       redirect_to root_path, notice: "Created successfully"
     else
--- spec/controllers/expenses_controller_spec.rb
+++ spec/controllers/expenses_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ExpensesController, type: :controller do
+  describe "GET #new" do
+    it "responds successfully with HTTP code 200" do
+      get :new
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
 
+  describe "POST #create" do
+    it "with only a positive value" do
+      post :create, expense: { value: 2 }
+      expect(Expense.last.value).to eq(2)
+    end
+  end
 end
